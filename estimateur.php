07d921585da86527948ae113d032edf2e88cced5
quentinNicolasTessier <quentin.nicolas.tessier@gmail.com>
2015-04-25 20:59:03 +0200

    commit 25/04/2015  20h58

--- estimateur.php
+++ estimateur.php
@@ -48,9 +48,14 @@
 		<section name="estimer" id="estimer"><?php 
 				/* appel de la fonction formulaireSub permettant d'afficher le formulaire avec les données slectionnées*/ 
 				formulaireSub($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);?>
-		</section><?php
-		/* appel de la fonction traitementDonnees permettant d'afficher l'estimation */
-		traitementDonnees($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);
+		</section>
+		<form name="nouvelleEst" id="nouvelleEst" action="http://localhost/simulateur_velux/estimateur.php" method="post" enctype="multipart/form-data">
+			<input type="hidden" name="nomPiece" id="nomPiece" value="<?php echo $_SESSION['nomFenetre'] ?>">
+			<input type="submit" name="boutSubNew" id="boutSubNew" value="Nouvelle Estimation">
+		</form>
+					<?php 
+			/* appel de la fonction traitementDonnees permettant d'afficher l'estimation */
+			traitementDonnees($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);
 	}
 	/* si on à pas cliquer sur le bouton estimer*/
 	else 
@@ -91,13 +96,19 @@
 			<section name="estimer" id="estimer">
 				<?php
 				/*verification que l'on à bien selectionnés les champs obligatoires*/
-				if($_SESSION['taille']=='0' && $_SESSION['finition'] && $_SESSION['ouverture'] && $_SESSION['type']){
+				if($_SESSION['taille']=='0' && $_SESSION['finition']=='0' && $_SESSION['ouverture']=='0' && $_SESSION['type']=='0' ){
 					formulaire();
-					traitementDonnees($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);
+					
 				}
 				else 
 				{
 					formulaireSub($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);
+					?>
+						<form name="nouvelleEst" id="nouvelleEst" action="http://localhost/simulateur_velux/estimateur.php" method="post" enctype="multipart/form-data">
+							<input type="hidden" name="nomPiece" id="nomPiece" value="<?php echo $_SESSION['nomFenetre'] ?>">
+							<input type="submit" name="boutSubNew" id="boutSubNew" value="Nouvelle Estimation">
+						</form>
+					<?php 
 					traitementDonnees($_SESSION['typeActuel'],$_SESSION['taille'],$_SESSION['finition'],$_SESSION['ouverture'],$_SESSION['type'],$_SESSION['raccord'],$_SESSION['volet'],$_SESSION['store'],$_SESSION['couleur'],$_SESSION['nomFenetre']);
 				}
 				?>
@@ -106,43 +117,54 @@
 		}
 		/* si on à pas cliquer sur le bouton renommer*/
 		else{
-				/*recuperation dans variables de sessions des données du formulaire permettant de renommer la piece*/
-				$_SESSION['typeActuel']='0';
-				$_SESSION['taille']='0';
-				$_SESSION['finition']='0';
-				$_SESSION['ouverture']='0';
-				$_SESSION['type']='0';
-				$_SESSION['raccord']='0';
-				$_SESSION['volet']='0';
-				$_SESSION['store']='0';
-				$_SESSION['couleur']='0';
-				$_SESSION['nomFenetre']="Nom de la piece";
-			?>
-			<!-- formulaire permettant de renommer la piece -->
-			<section name="renommer" id="renommer">
-				<form name="renommer" id="renommer" action="http://localhost/simulateur_velux/estimateur.php" method="post" enctype="multipart/form-data">
-					<span id='Formfenetre1'><?php echo $_SESSION['nomFenetre']?> :<input style="maxlength="100" type="text" name="fenetre1Nom"id="fenetre1Nom" 
-					placeholder="Renommer une piece"></span>
-					<input type="hidden" name="typeActuel" id="typeActuel" value="<?php echo $_SESSION['typeActuel'];?>">
-					<input type="hidden" name="taille" id="taille" value="<?php echo $_SESSION['taille'];?>">
-					<input type="hidden" name="finition" id="finition" value="<?php echo $_SESSION['finition'];?>">
-					<input type="hidden" name="ouverture" id="ouverture" value="<?php echo $_SESSION['ouverture'];?>">
-					<input type="hidden" name="type" id="type" value="<?php echo $_SESSION['type'];?>">
-					<input type="hidden" name="raccord" id="raccord" value="<?php echo $_SESSION['raccord'];?>">
-					<input type="hidden" name="volet" id="volet" value="<?php echo $_SESSION['volet'];?>">
-					<input type="hidden" name="store" id="store" value="<?php echo $_SESSION['store'];?>">
-					<input type="hidden" name="couleur" id="couleur" value="<?php echo $_SESSION['couleur'];?>"> 
-					<span>
-						<input style="width:100px;margin-left:5px;height:22px;font-size:10px;margin-top:15px;"
-						type="submit" name="fenetre1Boutton" id="fenetre1Boutton" value="renommer"/>
-					</span>
-					<br/>
-				</form>
-			</section>
-			<section>
-				<!-- appel de la fonction formulaire permettant d'afficher un formulaire d'estimation vide -->
-				<?php formulaire();?>
-			</section><?php 
+					
+					/*recuperation dans variables de sessions des données du formulaire permettant de renommer la piece*/
+					$_SESSION['typeActuel']='0';
+					$_SESSION['taille']='0';
+					$_SESSION['finition']='0';
+					$_SESSION['ouverture']='0';
+					$_SESSION['type']='0';
+					$_SESSION['raccord']='0';
+					$_SESSION['volet']='0';
+					$_SESSION['store']='0';
+					$_SESSION['couleur']='0';
+					if(isset($_POST['boutSubNew']))
+					{
+						$_SESSION['nomFenetre']=$_POST['nomPiece'];
+					}
+					else 
+					{
+						$_SESSION['nomFenetre']="Nom de la piece";
+					}
+					
+				?>
+				<!-- formulaire permettant de renommer la piece -->
+				<section name="renommer" id="renommer">
+					<form name="renommer" id="renommer" action="http://localhost/simulateur_velux/estimateur.php" method="post" enctype="multipart/form-data">
+						<span id='Formfenetre1'><?php echo $_SESSION['nomFenetre']?> :<input style="maxlength="100" type="text" name="fenetre1Nom"id="fenetre1Nom" 
+						placeholder="Renommer une piece"</span>
+						<input type="hidden" name="typeActuel" id="typeActuel" value="<?php echo $_SESSION['typeActuel'];?>">
+						<input type="hidden" name="taille" id="taille" value="<?php echo $_SESSION['taille'];?>">
+						<input type="hidden" name="finition" id="finition" value="<?php echo $_SESSION['finition'];?>">
+						<input type="hidden" name="ouverture" id="ouverture" value="<?php echo $_SESSION['ouverture'];?>">
+						<input type="hidden" name="type" id="type" value="<?php echo $_SESSION['type'];?>">
+						<input type="hidden" name="raccord" id="raccord" value="<?php echo $_SESSION['raccord'];?>">
+						<input type="hidden" name="volet" id="volet" value="<?php echo $_SESSION['volet'];?>">
+						<input type="hidden" name="store" id="store" value="<?php echo $_SESSION['store'];?>">
+						<input type="hidden" name="couleur" id="couleur" value="<?php echo $_SESSION['couleur'];?>"> 
+						<span>
+							
+							<input style="width:100px;margin-left:5px;height:22px;font-size:10px;margin-top:15px;"
+							type="submit" name="fenetre1Boutton" id="fenetre1Boutton" value="renommer"/>
+						</span>
+						<br/>
+					</form>
+				</section>
+				<section>
+					<!-- appel de la fonction formulaire permettant d'afficher un formulaire d'estimation vide -->
+					<?php formulaire();?>
+				</section><?php 
+			
 		}
 		
 		 
